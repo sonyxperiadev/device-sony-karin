b2fd6157183b377b37ad6c62a4379bb5f75ddaa5
David Viteri <davidteri91@gmail.com>
2016-01-03 14:44:43 +0100

    karin: drop TARGET_TAP_TO_WAKE_STRING
    it was used on shinano due to sirius path isn't using 1 : 0 for tap to wake feature... here this is not needed
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,4 +18,3 @@ include device/sony/common/CommonConfig.mk
 TARGET_BOOTLOADER_BOARD_NAME := SGP771
 
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/maxim_sti/gesture_wakeup"
-TARGET_TAP_TO_WAKE_STRING := true
